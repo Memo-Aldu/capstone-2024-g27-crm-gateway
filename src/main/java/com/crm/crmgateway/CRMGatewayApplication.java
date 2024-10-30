881258b6b9006449f0b88923d7f44c510974d219
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2024-10-30 16:10:23 -0400

    Inital gateway project

---
+++ src/main/java/com/crm/crmgateway/CRMGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.crm.crmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CRMGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CRMGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/crmgateway/CRMGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.crmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CRMGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
